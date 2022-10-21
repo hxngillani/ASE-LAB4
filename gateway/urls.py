c08f5aa3296d497bff48c92249bd8ad334eb6e81
hxngillani <hassangillani@outllok.com>
2022-10-21 18:58:10 +0200

    Running gateway service

---
+++ gateway/urls.py
@@ -0,0 +1,3 @@
+GATEWAY_URL = 'http://127.0.0.1:' + '5002'       # add <PORT1>
+MATH_URL = 'http://127.0.0.1:' + '5001'          # add <PORT2>
+STRING_URL = 'http://127.0.0.1:' + '5000'        # add <PORT3>
